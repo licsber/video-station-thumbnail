3c3fc821efc879632c85d96497da74aee3075140
licsber <silverwings@foxmail.com>
2020-05-14 00:06:38 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# video-station-Thumbnail
+
+## 使用说明
+
+Docker上下载这个镜像
+
+设置视频目录VOLUME为/video运行即可
+
+自动在同名文件下创建缩略图
---
+++ main.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+
+video_path = '/video'
+
+videos = []
+
+
+def walk(file):
+    global videos
+
+    if os.path.isdir(file):
+        for f in os.listdir(file):
+            walk(file + '/' + f)
+    else:
+        if file.endswith('.mp4'):
+            videos.append(file)
+
+
+walk(video_path)
+
+
+# print(videos)
+
+def get_key_frame(video):
+    cap = cv2.VideoCapture(video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    key = int(fps * 2) + 1
+    cap.set(cv2.CAP_PROP_POS_FRAMES, key)
+    _, frame = cap.read()
+    cap.release()
+    # cv2.imshow('key_frame', frame)
+    # cv2.waitKey(0)
+    return frame
+
+
+for video in videos:
+    name = video.rstrip('.mp4') + '.jpg'
+    frame = get_key_frame(video)
+    cv2.imwrite(name, frame)
+    print(name)
+
+if __name__ == '__main__':
+    pass
